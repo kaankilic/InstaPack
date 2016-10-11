6392a067062d085e38fb9bd66e60e35f2a3dc163
Kaan Kılıç <kaankilic@users.noreply.github.com>
2016-10-11 22:33:41 +0300

    Update InstallerController.php

--- src/Http/Controllers/InstallerController.php
+++ src/Http/Controllers/InstallerController.php
@@ -2,11 +2,21 @@
 namespace Kaankilic\InstaPack\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Input;
+use App\Models\Companies;
+use App\Models\Users;
 use Validator;
 use Storage;
+use Artisan;
+use Hash;
+use Session;
+use Request;
+use Kaankilic\InstaPack\Helpers\ExtensionChecker;
+use Kaankilic\InstaPack\Helpers\FilePermissionChecker;
+use Kaankilic\InstaPack\Helpers\DotEnvGen;
 class InstallerController extends Controller{
 	public function getHome(){
-		return view("instapack::installer.home");
+		$Extensions = ExtensionChecker::checkExtensions(config('instapack.Extensions'));
+		return view("instapack::installer.home",compact('Extensions'));
 	}
 	public function getConnections(){
 		return view("instapack::installer.connections");
@@ -16,13 +26,15 @@ class InstallerController extends Controller{
 		$Validator = Validator::make($Inputs,[
 			"Hostname" => "required",
 			"Username" => "required",
+			"Password" => "required",
 			"Database" => "required"
 		]);
-		if ($Validator->fails()) {
-			return redirect()->route("instapack::connections")->withInput()->withErros($Validator)->with("Errormessage","test");
+		$hasConnection = @mysqli_connect($Inputs["Hostname"],$Inputs["Username"],$Inputs["Password"],$Inputs["Database"]);
+		if ($Validator->fails() || $hasConnection==FALSE) {
+			return redirect()->route("instapack::connections")->withInput()->withErrors($Validator)->with("Errormessage","test");
 		}
+		Storage::disk("local")->put('instapack-connections.json', json_encode($Inputs));
 		return redirect()->route("instapack::mail");
-		dd(true);
 	}
 	public function getMail(){
 		return view("instapack::installer.mail");
@@ -35,24 +47,60 @@ class InstallerController extends Controller{
 			"Email" => "required"
 		]);
 		if ($Validator->fails()) {
-			return redirect()->route("instapack::mail")->withInput()->withErros($Validator)->with("Errormessage","test");
+			return redirect()->route("instapack::mail")->withInput()->withErrors($Validator)->with("Errormessage","test");
 		}
+		/* Smtp Test et */
+		Storage::disk("local")->put('instapack-emails.json', json_encode($Inputs));
 		return redirect()->route("instapack::user");
 	}
 	public function getUser(){
 		return view("instapack::installer.user");
 	}
 	public function postUser(){
+		$Company = Input::only("Title","Address1","Address2","ZipCode","City","Phone","Email","CountryID");
 		$Inputs = Input::only("Username","Password");
 		$Validator = Validator::make($Inputs,[
 			"Username" => "required",
 			"Password" => "required"
 		]);
-		if ($Validator->fails()) {
-			return redirect()->route("instapack::user")->withInput()->withErros($Validator)->with("Errormessage","test");
+		$hasConnection = Storage::disk('local')->exists('instapack-connections.json');
+		$hasEmailSettings = Storage::disk('local')->exists('instapack-emails.json');
+		if ($Validator->fails() || $hasConnection==FALSE || $hasEmailSettings==FALSE) {
+			return redirect()->route("instapack::user")->withInput()->withErrors($Validator)->with("Errormessage","test");
 		}
-		$Data = array("Result"=>str_random(40));
-		Storage::disk("local")->put('install.json', $Data);
+		$Company["isOwn"] = 1;
+		$Inputs["Password"] = Hash::make($Inputs["Password"]);
+		$Data = array(
+			"Status" => "Installed",
+			"ResultCode"=>str_random(40)
+		);
+		$Connection = json_decode(Storage::disk('local')->get('instapack-connections.json'));
+		$Emails = json_decode(Storage::disk('local')->get('instapack-emails.json'));
+		$deg = new DotEnvGen();
+		$deg->parseExample();
+		$deg->setField("APP_ENV","production");
+		$deg->setField("APP_DEBUG","false");
+		$deg->setField("APP_KEY",$Data["ResultCode"]);
+		$deg->setField("APP_URL",Request::root());
+		$deg->setField("DB_HOST",$Connection->Hostname);
+		$deg->setField("DB_PORT","3306");
+		$deg->setField("DB_DATABASE",$Connection->Database);
+		$deg->setField("DB_USERNAME",$Connection->Username);
+		$deg->setField("DB_PASSWORD",$Connection->Password);
+		$deg->setField("MAIL_DRIVER","smtp");
+		$deg->setField("MAIL_HOST",$Emails->Hostname);
+		$deg->setField("MAIL_PORT","587");
+		$deg->setField("MAIL_USERNAME",$Emails->Hostname);
+		$deg->setField("MAIL_PASSWORD",$Emails->Hostname);
+		$deg->setField("MAIL_ENCRYPTION","null");
+		$deg->createEnv(base_path()."/.env");
+		/* Migration and Seeds */
+		Artisan::call('migrate');
+		Artisan::call('db:seed');
+		Users::create($Inputs);
+		Companies::create($Company);
+		Storage::disk('local')->delete(['instapack-connections.json','instapack-emails.json']);
+		Storage::disk('local')->put('instapack.json',$Data);
 		return redirect("/");
 	}
 }
